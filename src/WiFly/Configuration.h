ea1c20e0d0bbf19dcddd5b13e9ee127994e85e9a
follower <none@none>
2010-12-15 00:30:39 +1300

    Add documentation to explain how to modify configuration value to match your shield revision and how to identify the revision of your shield.

--- src/WiFly/Configuration.h
+++ src/WiFly/Configuration.h
@@ -1,5 +1,56 @@
+/*
 
-#define SHIELD_REVISION 3
+  Configuration.h -- configuration of different board features
+
+  Different revisions of the WiFly shield support different
+  features. If you are using an older revision of the shield you will
+  need to modify the value of 'SHIELD_REVISION' below to indicate
+  which revision of the WiFly shield you are using.
+
+  The value defaults to the most recent revision sold at the time of
+  code release.
+
+
+  How to identify which shield revision you have
+  ----------------------------------------------
+
+  * Revision 3 (Define SHIELD_REVISION as the value 3)
+
+    + <http://www.sparkfun.com/products/9954>
+
+    + Has a date code "6/15/10" on the underside of the PCB near the
+      mounting hole nearest the WiFly module.
+
+    + Has the ~14MHz crystal and hardware reset support.
+
+
+  * Revision 2 (Define SHIELD_REVISION as the value 2)
+
+    + <http://www.sparkfun.com/products/9367> (Same page as revision 1.)
+
+    + Is identified by having the ~14MHz crystal (rectangular metal
+      device near pins 6 & 7 with "14." engraved on it) and *not* having
+      a date code on the underside of the PCB.
+
+    + Has the ~14MHz crystal but no hardware reset support.
+
+   
+  * Revision 1 (Define SHIELD_REVISION as the value 1)
+
+    + <http://www.sparkfun.com/products/9367> (Same page as revision 2.)
+
+    + Is identified by having the *~12MHz* crystal (rectangular metal
+      device near pins 6 & 7 with "12." engraved on it) and *not* having
+      a date code on the underside of the PCB.
+
+    + Has the ~12MHz crystal and no hardware reset support.
+  
+ */
+
+#define SHIELD_REVISION 3 // Change this value to match your shield revision
+
+
+/* -- Do not change anything below this line -- */
 
 #ifndef __CONFIGURATION_H__
 #define __CONFIGURATION_H__
