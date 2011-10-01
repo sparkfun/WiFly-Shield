10c4c33f0498c1d7cbc63b48c40ba26cf8fae4fa
John Crouchley <john@crouchley.com>
2011-10-01 15:36:33 +0100

    New example using the Ciseco XBee shield WiFly_PachubeClient_Ciseco

---
+++ src/WiFly/examples/WiFly_PachubeClient_Ciseco/Credentials.h
@@ -0,0 +1,15 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// supply your own Pachube feed ID
+#define PACHUBEFEED "36718"
+// this API key will only work from my IP address - you need to supply your own
+#define APIKEY "v1tYYhpjSApMwXsrdWYd5w2pj8_wSdgsd3R8odYqInM"
+#define TIMETOUPDATE 15000  // frequency of update - every 15 seconds
+
+// Wifi parameters
+char passphrase[] = "your passphrase here";
+char ssid[] = "your ssid";
+boolean mode = WPA_MODE; //or WEP_MODE
+
+#endif
